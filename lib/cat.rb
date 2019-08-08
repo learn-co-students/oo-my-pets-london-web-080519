088a8393f0d57eeb9bd7627488a73f5fdab06229
PollyCR <polly.c.reynolds@gmail.com>
2019-08-08 16:11:06 +0100

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
+require 'pry'
+require_relative 'owner.rb'
+require_relative 'dog.rb'
+
 class Cat
-  # code goes here
+attr_reader :name, :species 
+attr_accessor :owner, :mood 
+
+@@all_cats = []
+def initialize(name, owner)
+  @name = name
+  @owner = owner 
+  @mood = 'nervous'
+  @species = 'cat'
+  @@all_cats << self
+end 
+
+def self.all
+  @@all_cats 
+end 
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
+require 'pry'
+require_relative 'owner.rb'
+require_relative 'cat.rb'
 class Dog
-  # code goes here
-end
+  attr_reader :name, :species
+  attr_accessor :owner, :mood 
+  
+  @@all_dogs = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner 
+    @species = 'dog'
+    @mood = 'nervous'
+    @@all_dogs << self
+  end 
+  
+  def self.all
+    return @@all_dogs 
+  end 
+  
+  end
